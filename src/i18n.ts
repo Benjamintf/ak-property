e896e33c83ef5a61fc17430090e8ff30917e554b
Benjamin <tesfakifle76@gmail.com>
2026-05-02 14:40:35 -0900

    fixing folder structure and paths

---
+++ src/i18n.ts
@@ -0,0 +1,15 @@
+import {notFound} from 'next/navigation';
+import {getRequestConfig} from 'next-intl/server';
+
+// የሚፈቀዱ ቋንቋዎች ዝርዝር
+const locales = ['en', 'am'];
+
+export default getRequestConfig(async ({locale}) => {
+  // የቀረበው ቋንቋ በዝርዝሩ ውስጥ ካለ አረጋግጥ
+  if (!locales.includes(locale as any)) notFound();
+
+  return {
+    // ማሳሰቢያ፡ የ 'messages' ፎልደር ከ 'src' ውጭ ከሆነ '../' ተጠቀም
+    messages: (await import(`../messages/${locale}.json`)).default
+  };
+});
