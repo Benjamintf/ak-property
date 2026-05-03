c0dda5ca9983fcb5ca2703f217deacfbd19add8f
Benjamin <tesfakifle76@gmail.com>
2026-05-03 14:17:55 -0900

    Update globe icon name

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -3,7 +3,7 @@
 import React, { useState } from 'react';
 import { useTranslations } from 'next-intl';
 import { useRouter, useParams } from 'next/navigation';
-import { MessageCircle, Send, FacebookIcon, Phone, Search, MapPin, Home as HomeIcon, Tag } from 'lucide-react';
+import { MessageCircle, Send, Globe, Phone, Search, MapPin, Home as HomeIcon, Tag } from 'lucide-react';
 
 export default function Home() {
   const t = useTranslations('HomePage');
@@ -223,7 +223,7 @@ export default function Home() {
               <div className="flex justify-center md:justify-start gap-6">
                 <SocialIcon href="https://t.me/PropertiesInEthiopia" icon={<Send size={20} />} label="Telegram" />
                 <SocialIcon href="https://wa.me/251913739983" icon={<MessageCircle size={20} />} label="WhatsApp" />
-                <SocialIcon href="https://www.facebook.com/..." icon={<FacebookIcon size={20} />} label="Facebook" />
+                <SocialIcon href="https://www.facebook.com/..." icon={<Globe size={20} />} label="Facebook" />
               </div>
             </div>
             
