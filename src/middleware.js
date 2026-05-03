32c105cdbede133467cd0db20bd88688299efefd
Benjamin <tesfakifle76@gmail.com>
2026-05-03 01:18:04 -0900

    Corrected messages path and config

--- src/middleware.js
+++ src/middleware.js
@@ -1,14 +1,10 @@
 import createMiddleware from 'next-intl/middleware';
  
 export default createMiddleware({
-  // የምንጠቀማቸው ቋንቋዎች ዝርዝር
   locales: ['en', 'am'],
- 
-  // መጀመሪያ ዌብሳይቱ ሲከፈት የሚታየው ቋንቋ
   defaultLocale: 'en'
 });
  
 export const config = {
-  // እነዚህን መንገዶች ብቻ ነው ቋንቋ የሚቀይረው
   matcher: ['/', '/(am|en)/:path*']
 };
